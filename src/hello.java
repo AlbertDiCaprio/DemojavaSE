5edc6582a524bdd408af96e5e2e1fcacd7aa4dc1
AlbertDiCaprio <493842405@qq.com>
2016-06-20 13:40:21 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.Soundbank;
+
+/**
+ * Created by Administrator on 2016/6/20.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
---
+++ src/text.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/6/20.
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("你好吗？");
+    }
+}
